b6cf92103d3f845f259567a168a025edc977a6cc
Griffin <griffin.z.s@outlook.com>
2024-02-26 01:59:59 +0000

    add failsafe for apks folder not existing

---
+++ downloadAPKs.py
@@ -0,0 +1,15 @@
+from urllib.request import urlretrieve
+
+githubAPKs = {
+    "obtanium": "https://api.github.com/repos/ImranR98/Obtainium/releases",
+    ""
+}
+
+gplayAPKs = {
+    "melonds":
+
+}
+
+if not os.path.exists("apks"):
+    os.mkdir("apks")
+    
