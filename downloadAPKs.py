3f4e0a311645cc35936787e62f391df60ca1e028
Griffin <griffin.z.s@outlook.com>
2024-03-03 20:19:20 +0000

    add more emus

--- downloadAPKs.py
+++ downloadAPKs.py
@@ -9,6 +9,8 @@ githubAPKs = {
     "yuzu": "yuzu-emu/yuzu-android",
     "vita3k": "Vita3K/Vita3K-Android",
     "melonds": "rafaelvcaetano/melonDS-android",
+    "flycast": "flyinghead/flycast",
+    "skyemu": "skylersaleh/SkyEmu",
 }
 
 otherAPKs = {
@@ -20,6 +22,11 @@ gplayAPKs = {
     "dolphin": "org.dolphinemu.dolphinemu",
     "duckstation": "com.github.stenzek.duckstation",
     "PPSSPP": "org.ppsspp.ppsspp",
+    "Snes9x EX+": "com.explusalpha.Snes9xPlus",
+    "Yaba Sanshiro": "org.devmiyax.yabasanshioro2",
+    "M64Plus FZ": "org.mupen64plusae.v3.fzurita",
+    "Redream": "io.recompiled.redream",
+
 }
 
 def downloadAPKs():
