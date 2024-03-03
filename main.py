e72582511d920514ebe6ecc8f693e131ca215809
Griffin <griffin.z.s@outlook.com>
2024-03-03 00:27:38 +0000

    failsafes and folder pruning

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ import nethersx2patch
 import downloadAPKs
 import helperfunctions
 
+# Ensure adb is installed
+try:
+    os.system("adb version")
+except:
+    print("adb not installed. Please install it before running this program: https://developer.android.com/tools/releases/platform-tools#downloads")
+    exit()
+
 # Initialize adb
 adb = adbutils.AdbClient(host="127.0.0.1", port=5037)
 for info in adb.list():
@@ -38,5 +45,8 @@ for apk in os.listdir("apks"):
 
 downloadAPKs.installPlayStore(d)
 
-# Copy the folder structure over
-os.system("adb push Emulation.zip /storage/emulated/0")
+# Copy the folder structure over (for some reason the python package doesn't work)
+os.system("adb push Emulation /storage/emulated/0")
+
+# Copy obtainium json to downloads to be uploaded
+d.sync.push("obtainium-emus.json", "/storage/emulated/0/Downloads/obtainium-emus.json")
