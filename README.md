9b693f8a9b7f92c0d9f8123136cc0b5ac3248084
Griffin <griffin.z.s@outlook.com>
2024-03-03 02:22:21 +0000

    update readme mostly

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Emudroid-Installer
+
+A simple to use python script to install popular emulators on your Android device.
+
+## Prerequisites
+
+* [Python 3](https://www.python.org/downloads/)
+* [adb](https://developer.android.com/tools/releases/platform-tools#downloads)
+    * Confirm that you have this before installing.
+* An Android device of decent recency
+    * I am not sure exactly which version is required, as all apps have different requirements.
+
--- main.py
+++ main.py
@@ -63,10 +63,7 @@ downloadAPKs.installPlayStore(d)
 os.system("adb push Emulation /storage/emulated/0")
 
 # Copy obtainium json to downloads to be uploaded
-d.sync.push("obtainium-config.json", "/storage/emulated/0/Downloads/obtainium-config.json")
 print(f"Opening browser to Obtainium config downloader. Please add the Obtainium configs for the desired applications.")
-print(f"Alternatively, import the file 'obtainium-config.json' that has been downloaded to the /Downloads/ folder.")
-shellcmd=f"am start -a android.intent.action.VIEW -d https://wingofagriffin.github.io/Emudroid-Installer/"
 d.shell(shellcmd)
 print("Press Enter to continue...")
 input()
